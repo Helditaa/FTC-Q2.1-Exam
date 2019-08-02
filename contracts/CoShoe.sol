c96838dd26db0e1bb2b273c5bead4259fda05475
Helda Mandlate <mndhel002@myuct.ac.za>
2019-08-02 21:04:25 +0200

    Added full contract

--- contracts/CoShoe.sol
+++ contracts/CoShoe.sol
@@ -16,6 +16,24 @@ contract CoShoe {
 
     constructor() public {
         shoes.push(Shoe(msg.sender, "","",false));
+        // mint 100 tokens
     }
 
+    function buyShoe(string memory _name, string memory _image) public payable { //used to be external payable
+        require(shoes.length > 0, "shoes array is empty");
+        require(msg.value == price, "value is not equal to value");
+
+        shoes.push(Shoe(msg.sender, _name, _image, true));
+        shoesSold++;
+    }
+
+    function checkPurchases() external view returns (bool[] memory){
+        bool[] memory checkPurchase;
+        for (uint i = 0; i < shoes.length; i++){
+            if (shoes[i].owner == msg.sender){
+                checkPurchase[i] = true;
+            }
+        }
+        return checkPurchase;
+    } 
 }
