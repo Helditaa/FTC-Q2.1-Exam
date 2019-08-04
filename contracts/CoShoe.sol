5880d6e0cc33200435c5514029f7ddfb204d8b53
Helda Mandlate <mndhel002@myuct.ac.za>
2019-08-04 13:02:54 +0200

    Added CoShoe

--- contracts/CoShoe.sol
+++ contracts/CoShoe.sol
@@ -21,27 +21,22 @@ contract CoShoe {
         }
     }
 
-    function getNumberofTokens() public view returns(uint count) {
+    function getNumberofTokens() public view returns(uint counts) {
         return shoes.length;
     }
 
     function buyShoe(string memory _name, string memory _image) public payable { //used to be external payable
-        //require(shoes.length >0, "Shoes array is full");
-        //require(Shoe.sold == true)
-        require(msg.value == price, "Value is not equal to price");
 
-        for (uint256 i = 0; i < shoes.length; i++) {
-        if(shoes[i].sold == false)
-            {
-                shoes[i].owner = msg.sender;
-                shoes[i].name = _name;
-                shoes[i].image = _image;
-                shoes[i].sold = true;
-                shoes.push(Shoe(shoes[i].owner, shoes[i].name,shoes[i].image,shoes[i].sold));
-                shoesSold++;
-                
-            } 
-        }
+        require(shoesSold < shoes.length, "exceeded number of shoes");
+        require(msg.value == price, "Value is not equal to price");
+        uint256 ii = shoesSold + 1;
+    
+        shoes[ii].owner = msg.sender;
+        shoes[ii].name = _name;
+        shoes[ii].image = _image;
+        shoes[ii].sold = true;
+        //shoes.push(Shoe(shoes[ii].owner, shoes[ii].name,shoes[ii].image,shoes[ii].sold));
+        shoesSold++;
 
     }
 
